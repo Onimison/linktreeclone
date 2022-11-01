9ea64f976de2066d3a0e5e17eaf4fb5dab27a14b
Onimison <godblessbashir@gmail.com>
2022-11-01 01:32:35 +0100

    modified comits

--- src/components/Links/links.js
+++ src/components/Links/links.js
@@ -9,21 +9,21 @@ const links = () => {
   return (
     <section className="flex flex-col gap-4 items-center">
 
-      <a href="https://training.zuri.team/" target="_blank" id='btn_zuri'>
-        <button className="bg-gray w-full max-w-[50rem] p-4 rounded-lg text-center drop-shadow-[0_1px_2px_rgba(16,24,40,0.05)]">Twitter Link</button>
+      <a href="https://training.zuri.team/" target="_blank" id='btn_zuri' className="bg-gray w-full max-w-[50rem] p-4 rounded-lg text-center drop-shadow-[0_1px_2px_rgba(16,24,40,0.05)]">
+        <button >Twitter Link</button>
       </a>
 
-      <a href="http://books.zuri.team/" target="_blank" id='books'>
-        <button className="bg-gray w-full max-w-[50rem] p-4 rounded-lg text-center drop-shadow-[0_1px_2px_rgba(16,24,40,0.05)]">Zuri Books</button>
+      <a href="http://books.zuri.team/" target="_blank" id='books' className="bg-gray w-full max-w-[50rem] p-4 rounded-lg text-center drop-shadow-[0_1px_2px_rgba(16,24,40,0.05)]">
+        <button >Zuri Books</button>
       </a>
-      <a href="https://books.zuri.team/" target="_blank" id='book_python'>
-        <button className="bg-gray w-full max-w-[50rem] p-4 rounded-lg text-center drop-shadow-[0_1px_2px_rgba(16,24,40,0.05)]">Python Books</button>
+      <a href="https://books.zuri.team/" target="_blank" id='book_python' className="bg-gray w-full max-w-[50rem] p-4 rounded-lg text-center drop-shadow-[0_1px_2px_rgba(16,24,40,0.05)]">
+        <button >Python Books</button>
       </a>
-      <a href="https://background.zuri.team/" target="_blank" id='pitch'>
-        <button className="bg-gray w-full max-w-[50rem] p-4 rounded-lg text-center drop-shadow-[0_1px_2px_rgba(16,24,40,0.05)]">Background Checks for Coders</button>
+      <a href="https://background.zuri.team/" target="_blank" id='pitch' className="bg-gray w-full max-w-[50rem] p-4 rounded-lg text-center drop-shadow-[0_1px_2px_rgba(16,24,40,0.05)]">
+        <button >Background Checks for Coders</button>
       </a>
-      <a href="https://books.zuri.team/design-rules" target="_blank" id='book_design'>
-        <button className="bg-gray w-full max-w-[50rem] p-4 rounded-lg text-center drop-shadow-[0_1px_2px_rgba(16,24,40,0.05)]">Design Books</button>
+      <a href="https://books.zuri.team/design-rules" target="_blank" id='book_design' className="bg-gray w-full max-w-[50rem] p-4 rounded-lg text-center drop-shadow-[0_1px_2px_rgba(16,24,40,0.05)]">
+        <button >Design Books</button>
       </a>
 
       <div className="flex justify-center gap-4 h-14 p-4">
