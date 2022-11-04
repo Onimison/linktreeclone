cd7b69febdd5581bf89db24738fdc1caa1066fd9
Onimison <godblessbashir@gmail.com>
2022-11-04 01:40:01 +0100

    feat: Added the routing feature of the contact link to the Index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+} from "react-router-dom";
+
 import App from './App';
+import Contact from './components/Contact/contact';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/contact",
+        element: <Contact />
+    }
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+)
