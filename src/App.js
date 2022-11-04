aab4248d7d59455f5ef5f7d4782b0756263b08af
Onimison <godblessbashir@gmail.com>
2022-11-04 01:36:12 +0100

    fix: cleaned up the code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import Profile from './components/Profile/profile'
-import Links from './components/Links/links'
-import Footer from './components/Footer/footer'
-const App = () => {
+import React from 'react';
+// import { createBrowserRouter, RouterProvider, Router } from 'react-router-dom';
+import Profile from './components/Profile/profile';
+import Links from './components/Links/links';
+import Footer from './components/Footer/footer';
+import Contact from './components/Contact/contact';
 
+
+
+const App = () => {
     return (
         <>
+
             <Profile />
             <Links />
             <Footer />
